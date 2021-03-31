b1f7a66a39c92208e47052eef0abbc7c8a3b9421
ngoc.anh <ngocanhphamk2@gmail.com>
2021-03-31 20:33:36 +0700

    Initial commit

---
+++ src/bot.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const { Client } = require('discord.js')
+
+const client = new Client();
+
+client.on('ready', () => {
+  console.log(`${client.user.username} has logged in`);
+})
+
+client.on('message', (msg) => {
+  if (msg.author.bot) {
+    return;
+  }
+
+  if (msg.content === 'hello bot') {
+    msg.reply('lo con cac')
+  }
+})
+
+client.login(process.env.DISCORD_BOT_TOKEN)
