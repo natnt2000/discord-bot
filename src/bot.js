a037c61eb80ca4f2d93566fa3b90c7c668b961ef
natnt2000 <59156692+natnt2000@users.noreply.github.com>
2021-04-01 23:19:45 +0700

    add new alert content

--- src/bot.js
+++ src/bot.js
@@ -16,6 +16,10 @@ client.on('message', (msg) => {
   if (msg.content === 'hello bot') {
     msg.reply('lo con cac')
   }
+  
+  if (msg.content === 'lâm tám') {
+    msg.reply('Lâm Tám tên thật là Trần Quang Lâm, sinh ngày 20/12/2000, tuổi lồn !');
+  }
 })
 
 client.login(process.env.DISCORD_BOT_TOKEN)
